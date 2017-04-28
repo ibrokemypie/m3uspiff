4120b8624c5d4c6ed9bcecd34d7fe5b02e282813
ibrokemypie <ibrokemypie@bastardi.net>
2017-04-28 11:04:21 +1000

    fix final pep8 issues

--- m3uspiff.py
+++ m3uspiff.py
@@ -4,33 +4,36 @@ import sys
 import subprocess
 from lxml import etree
 
-#Require one argument
+# Require one argument
 if len(sys.argv) != 2:
     print str(sys.argv)
     raise NameError('Please enter ONE argument')
+
 
 def parse_m3u(m3ufile, playlist):
     """Reads the lines of the input file"""
     track_list = etree.SubElement(playlist, "trackList")
     for line in open(m3ufile):
-        #strip any of the extended m3u ickiness
+        # strip any of the extended m3u ickiness
         if not line.lstrip().startswith('#'):
-            #create "track" subtree
+            # create "track" subtree
             track_element = etree.SubElement(track_list, "track")
-            #add location straight away
+            # add location straight away
             location_element = etree.SubElement(track_element, "location")
             location_element.text = line.rstrip()
-            #now find the other tags
+            # now find the other tags
             mdata(line, track_element)
+
 
 def mdata(path, track_element):
     """Find the metadata of each accesible file, and format as XML."""
-    #Define list of tags to search for
+    # Define list of tags to search for
     tags = {"title", "artist", "album", "genre", "recording date", "label"}
-    #Define ffprobe syntax
+    # Define ffprobe syntax
     cmd = ['ffprobe', path.rstrip()]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #Main loop
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    # Main loop
     while True:
         out = process.stdout.readline()
         if out != b'':
@@ -49,12 +52,15 @@ def mdata(path, track_element):
         else:
             break
 
+
 def write_file(m3ufile, playlist):
     """Saves the string of the XML to the new XSPF file."""
-    tree = etree.tostring(playlist, xml_declaration=True, encoding="utf-8", pretty_print=True)
+    tree = etree.tostring(playlist, xml_declaration=True, encoding="utf-8",
+                          pretty_print=True)
     new_file = open(m3ufile+".xspf", "w")
     new_file.write(tree)
     new_file.close()
+
 
 def main():
     """Main function"""
@@ -64,5 +70,6 @@ def main():
     argument = sys.argv[1]
     parse_m3u(argument, playlist)
     write_file(argument, playlist)
+
 
 main()
